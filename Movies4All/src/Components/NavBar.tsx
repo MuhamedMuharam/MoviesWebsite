7b42136a2483bcdfb149180d5c4f6e9c46d3fe04
Muhamed <muhamedmuharam1411@gmail.com>
2025-04-04 02:21:57 +0200

    Edit NavBar

--- Movies4All/src/Components/NavBar.tsx
+++ Movies4All/src/Components/NavBar.tsx
@@ -2,23 +2,17 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 function NavBar() {
-  const handleHomeClick = () => {
-    window.location.href = "/"; // This will force the page to reload and go to the Home page
-  };
 
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        {/* "Movies4ALL" clickable logo */}
-        <span className="nav-logo" onClick={handleHomeClick}>
-          Movies4ALL
-        </span>
-      </div>
+                <Link to="MoviesWebsite/">Movies4ALL</Link>
+            </div>
       <div className="navbar-links">
         {/* Home link - clickable */}
-        <span className="nav-link home-link" onClick={handleHomeClick}>
+        <Link to="MoviesWebsite/" className="nav-link home-link">
           Home
-        </span>
+        </Link>
         {/* Favourites link - React Router Link */}
         <Link to="/favourites" className="nav-link favourites-link">
           Favourites
